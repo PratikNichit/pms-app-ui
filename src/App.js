98283fa33130177c148a8a1e581580ab3a667b42
Pratik Nichit <nichitpratik2004@gmail.com>
2024-08-12 21:48:03 +0530

    Set up basic routes and initialize boilerplate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import { Router, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/LoginPage" element={<LoginPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+import React from "react";
+const LoginPage = () => {
+  return <Typography variant="h2">Login Page</Typography>;
+};
+
+export default LoginPage;
